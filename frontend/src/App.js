c2c52a93ee6acce88af7bd1e0a568ef9ee4307dd
Vignesh <joshivignesh@users.noreply.github.com>
2021-10-26 22:24:56 +0530

    Changed Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     Hello world, Farm Stack Application
+    </div>
+  );
+}
+
+export default App;
