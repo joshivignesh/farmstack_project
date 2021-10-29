f0456f089bbbc8c7d46254c692eeb4973109ea07
Vignesh <joshivignesh@users.noreply.github.com>
2021-10-30 01:00:40 +0530

    Added Todo files

---
+++ frontend/src/components/Todo.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+import react from "react";
+
+ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+
+
+  export default Todo
